8a275e18ae1a3bc0b3a4fc30926ff0bcb934df3e
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-07 21:57:10 +0530

    Add files via upload

---
+++ lab5.5_14.cpp
@@ -0,0 +1,19 @@
+//inlude libray
+#include<iostream>
+using namespace std;
+//declare main function
+int main(){
+//declare variables
+          int a,b;
+          //applay conditions&displaying pattern
+          for(a=1;a<6;++a){
+                          for(b=1;b<6;b++){
+                                           if(b>=a)
+                                           cout<<"*";
+                                           else
+                                           cout<<" ";}
+                          cout<<"\n";}
+//closing programme
+return 0;
+}
+
