37ac5ae6f813b0f7e5347eec2068a0e60442808c
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-07 21:42:36 +0530

    Update lab5.5_10.cpp

--- lab5.5_10.cpp
+++ lab5.5_10.cpp
@@ -7,7 +7,7 @@ int main(){
           int a,b;
           //applay conditions&displaying pattern
           for(a=1;a<6;++a){
-                          for(b=1;b<10;b++){
+                          for(b=1;b<6;b++){
                                            if(a+b>5&&b<6)
                                            cout<<"*";
                                            else
