9aa14e2be5995b162843ab2b11a2d2437ff69a83
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-07 19:16:17 +0530

    Add files via upload

---
+++ lab5.5_7.cpp
@@ -0,0 +1,19 @@
+//inlude libray
+#include<iostream>
+using namespace std;
+//declare main function
+int main(){
+//declare variables
+          int a,b,c=a+5;
+          //applay conditions&displaying pattern
+          for(a=1;a<6;++a){
+                          for(b=1;b<10;b++){
+                                           if(a+b==2*a||(a+b==(4+2*a)&&b==a+4)||a==1&&b<6||a==5&&b>4)
+                                           cout<<"*";
+                                           else
+                                           cout<<" ";}
+                          cout<<"\n";}
+//closing programme
+return 0;
+}
+
