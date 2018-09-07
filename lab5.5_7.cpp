32584f62c767a1148331190233134c5ce443c2fb
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-07 21:46:00 +0530

    Update lab5.5_7.cpp

--- lab5.5_7.cpp
+++ lab5.5_7.cpp
@@ -6,7 +6,7 @@ int main(){
 //declare variables
           int a,b;
           //applay conditions&displaying pattern
-          for(a=1;a<6;++a){
+          for(a=1;a<10;++a){
                           for(b=1;b<6;b++){
                                            if(a+b==2*a||(a+b==(4+2*a)&&b==a+4)||a==1&&b<6||a==5&&b>4)
                                            cout<<"*";
