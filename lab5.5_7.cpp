aa215f75aaf78f4f941c837b3972074f5ad9d6cb
SreeragTK <42139412+SreeragTK@users.noreply.github.com>
2018-09-07 21:43:30 +0530

    Update lab5.5_7.cpp

--- lab5.5_7.cpp
+++ lab5.5_7.cpp
@@ -4,10 +4,10 @@ using namespace std;
 //declare main function
 int main(){
 //declare variables
-          int a,b,c=a+5;
+          int a,b;
           //applay conditions&displaying pattern
           for(a=1;a<6;++a){
-                          for(b=1;b<10;b++){
+                          for(b=1;b<6;b++){
                                            if(a+b==2*a||(a+b==(4+2*a)&&b==a+4)||a==1&&b<6||a==5&&b>4)
                                            cout<<"*";
                                            else
